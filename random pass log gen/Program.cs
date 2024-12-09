8eaf8a42cfd7153a6a9a4c3c8cfa5d0c349de5f1
lvd. <nbikiti@bk.ru>
2024-12-09 16:42:30 +0400

    +Login, +Choise

--- random pass log gen/Program.cs
+++ random pass log gen/Program.cs
@@ -3,17 +3,71 @@
 string[] rndstrLang = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
 //string[] rndstrSimv = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
 
+Console.Write("Выберите какой тип генерации вам подходит (1 - only Login | 2 - only Pass | 3 - All) : ");
+int input = Convert.ToInt32(Console.ReadLine());
 
-
-
-Console.Write("Укажите кол-во букв в пароле: ");
-int tryes = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < tryes; i++)
+switch (input)
 {
-    int rndnumgen = random.Next(0, 25);
-    string randmassive = rndstrLang[rndnumgen];
+    case 1:
+        
+        Console.Write("Укажите кол-во букв в Логине: ");
+        int tryesLog = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < tryesLog; i++)
+        {
+            int rndnumgen = random.Next(0, 25);
+            string randmassive = rndstrLang[rndnumgen];
     
-    Console.Write(randmassive);
+            Console.Write(randmassive);
+        }
+        
+        break;
+    
+    case 2:
+        
+        Console.Write("Укажите кол-во букв в пароле: ");
+        int tryesPass = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < tryesPass; i++)
+        {
+            int rndnumgen = random.Next(0, 25);
+            string randmassive = rndstrLang[rndnumgen];
+    
+            Console.Write(randmassive);
+        }
+        
+        break;
+    
+    case 3:
+        
+        Console.Write("Укажите кол-во букв в Логине: ");
+        tryesLog = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Укажите кол-во букв в пароле: ");
+        tryesPass = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Ваш Логин: ");
+        for (int i = 0; i < tryesLog; i++)
+        {
+            int rndnumgen = random.Next(0, 25);
+            string randmassive = rndstrLang[rndnumgen];
+    
+            Console.Write(randmassive);
+        }
+        
+        Console.Write("\nВаш Пароль: ");
+        for (int i = 0; i < tryesPass; i++)
+        {
+            int rndnumgen = random.Next(0, 25);
+            string randmassive = rndstrLang[rndnumgen];
+    
+            Console.Write(randmassive);
+        }
+        
+        break;
+    
+    default:
+        Console.WriteLine("Вы ввели неверный символ.");
+        break;
 }
 
--- random pass log gen/obj/Debug/net9.0/random pass log gen.AssemblyInfo.cs
+++ random pass log gen/obj/Debug/net9.0/random pass log gen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("random pass log gen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c9e36be6522a4847c982bf19742c368cd56e11")]
 [assembly: System.Reflection.AssemblyProductAttribute("random pass log gen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("random pass log gen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
